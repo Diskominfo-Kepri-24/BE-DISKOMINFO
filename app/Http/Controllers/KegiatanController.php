a2d58ca7ea144972118ec3f484019340f3a6e9e0
Sahra-Fatimah-Yasir <2101020084@student.umrah.ac.id>
2024-08-12 16:51:27 +0700

    fix: kegiatan controller

--- app/Http/Controllers/KegiatanController.php
+++ app/Http/Controllers/KegiatanController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Kegiatan;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class KegiatanController extends Controller
 {
@@ -17,22 +16,13 @@ class KegiatanController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            // 'user_id' => 'required|exists:users,id',
+            'user_id' => 'required|exists:users,id',
             'tanggal' => 'required|date',
             'catatan' => 'required|string',
         ]);
 
-        // kalo kegiatan mau 1 kali aja diisi pake bawah ini buat validasi
-        // $cekKegiatan = Kegiatan::query()->where("tanggal", $request->tanggal)->where('user_id', Auth::user()->id)->first();
-        // if(!is_null($cekKegiatan)){
-        //     return response()->json([
-        //         "status" => "Kegiatan hari ini sudah dikirim"
-        //     ], 400);
-        // }
-
         $kegiatan = Kegiatan::create([
-            // 'user_id' => $request->user_id,
-            'user_id' => Auth::user()->id,  // diambil dari id user yang sedang login
+            'user_id' => $request->user_id,
             'tanggal' => $request->tanggal,
             'catatan' => $request->catatan,
             'status' => 'Menunggu',
@@ -41,10 +31,9 @@ class KegiatanController extends Controller
         return response()->json($kegiatan, 201);
     }
 
-    public function show($id)
+    public function show($userId)
     {
-        // $kegiatan = Kegiatan::findOrFail($id);
-        $kegiatan = Kegiatan::where('user_id', Auth::user()->id)->get();
+        $kegiatan = Kegiatan::where('user_id', $userId)->get();
         return response()->json($kegiatan);
     }
 
@@ -54,8 +43,7 @@ class KegiatanController extends Controller
             'catatan' => 'required|string',
         ]);
 
-        // $kegiatan = Kegiatan::findOrFail($id);
-        $kegiatan = Kegiatan::findOrFail(Auth::user()->id);
+        $kegiatan = Kegiatan::findOrFail($id);
         $kegiatan->update([
             'catatan' => $request->catatan,
         ]);
